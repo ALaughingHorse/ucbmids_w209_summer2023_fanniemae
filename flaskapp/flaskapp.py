5ef050dc2adc0a4151ac99357ffb7a00d441396f
suvvas <83439587+suvvas@users.noreply.github.com>
2023-07-20 16:53:46 +0530

    changes to html pages and new results page

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def flaskapp():
-    file="about9.jpg"
-    return render_template("main_page.html",file=file)
+    return render_template("fannie-mae-viewer.html")
 
 @app.route("/riskanalyzer")
 def riskanalyzer():
-	return render_template("risk-analyzer.html")
-	
+    return render_template("risk-analyzer.html")
+
+@app.route("/getrisk", methods=["GET", "POST"])
+def getrisk():
+    return render_template("risk-analyzer-results.html")
+
+@app.route("/exploration")
+def exploration():
+    return render_template("data-viz-exploration.html")
+		
 if __name__ == "__main__":
     app.run()
